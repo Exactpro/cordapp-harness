4580bcb3a51847bc6efe33d4203054445ce0aa84
mz0 <mz@exactpro.com>
2019-10-02 13:13:41 +0300

    use '. runABN.sh' instead of build/nodes/runnodes
    runnodes starts a node with an argument like this:
      -Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,
         server=y,suspend=n,address=5007 ...
    
    If IDEA is running at the same time at least one node will
    fail to start with runnodes
    
    runABN.sh uses tmux ('brew install tmux' in macOS)

--- runABN.sh
+++ runABN.sh
@@ -1,5 +1,10 @@
+nodes="PartyA PartyB Notary"
 
-#   tmux new-window -n  PartyA      '/usr/lib/jvm/java-8-oracle/jre/bin/java' '-Dname=PartyA' \
+for node in $nodes; do
+    tmux new-window -n $node java -Dcapsule.jvm.args="-Xmx512m -XX:+UseG1GC" -jar build/nodes/${node}/corda.jar -b build/nodes/${node} ;\
+  [ $? -eq 0 -o $? -eq 143 ] || sh
+done
+# '-Dname=PartyA' \
 # '-Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5008 -javaagent:drivers/jolokia-jvm-1.6.0-agent.jar=port=7008,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter' \
 # '-jar' '/home/mz0/R3/cordapp-sample-obligation/kotlin-source/build/nodes/PartyA/corda.jar'; \
 # [ $? -eq 0 -o $? -eq 143 ] || sh
